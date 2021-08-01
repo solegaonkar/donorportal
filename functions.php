6082867ac44eaa8cfb871a17b7ba9b217f676fa4
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2021-08-01 12:25:48 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,6 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'portal');
 
+include('environment.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
